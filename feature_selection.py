a9c1755a7e055c77b47c86a07ffc2be06256a016
Vamsi-Cloudangles <111474104+Vamsi-Cloudangles@users.noreply.github.com>
2023-01-24 12:35:59 +0530

    Update feature_selection.py

--- feature_selection.py
+++ feature_selection.py
@@ -7,6 +7,9 @@ def feature_selection():
     print(dataset.head())
     dataset.drop(["SEX","PREGNANT","COPD","ASTHMA","INMSUPR","OTHER_DISEASE","CARDIOVASCULAR","OBESITY","TOBACCO"], axis=1, inplace=True)
     print(dataset)
+    dataset.to_csv('cleaned.csv')
+    print("DVC process start")
+    dvc()
     return dataset
 
 
